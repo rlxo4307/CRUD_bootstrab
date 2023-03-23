06f723fb4ba60aba6ae3c3cc95e666abcd666a65
rlxo4307 <rlxo4307.diquest.com>
2023-03-23 11:47:02 +0900

    feat(all): 프로젝트 초본

---
+++ src/main/java/org/hdcd/service/CoinService.java
@@ -0,0 +1,17 @@
+package org.hdcd.service;
+
+import java.util.List;
+
+import org.hdcd.domain.ChargeCoin;
+import org.hdcd.domain.PayCoin;
+
+public interface CoinService {
+
+	public void charge(ChargeCoin chargeCoin) throws Exception;
+
+	public List<ChargeCoin> list(Long userNo) throws Exception;
+	
+	public List<PayCoin> listPayHistory(Long userNo) throws Exception;
+
+	public List<ChargeCoin> personalList(Long userNo) throws Exception;
+}
---
+++ src/main/java/org/hdcd/service/CoinServiceImpl.java
@@ -0,0 +1,92 @@
+package org.hdcd.service;
+
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hdcd.domain.ChargeCoin;
+import org.hdcd.domain.Member;
+import org.hdcd.domain.PayCoin;
+import org.hdcd.repository.ChargeCoinRepository;
+import org.hdcd.repository.MemberRepository;
+import org.hdcd.repository.PayCoinRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CoinServiceImpl implements CoinService {
+
+	private final ChargeCoinRepository chargeCoinRepository;
+	
+	private final PayCoinRepository payCoinRepository;
+	
+	private final MemberRepository memberRepository;
+	
+	@Transactional
+	@Override
+	public void charge(ChargeCoin chargeCoin) throws Exception {
+		Member memberEntity = memberRepository.getOne(chargeCoin.getUserNo());
+		
+		int coin = memberEntity.getCoin();
+		int amount = chargeCoin.getAmount();
+		
+		memberEntity.setCoin(coin + amount);
+	
+		memberRepository.save(memberEntity);
+		
+		chargeCoinRepository.save(chargeCoin);
+	}
+
+	@Override
+	public List<ChargeCoin> list(Long userNo) throws Exception {
+		return chargeCoinRepository.findAll(Sort.by(Direction.DESC, "historyNo"));
+	}
+
+	@Override
+	public List<ChargeCoin> personalList(Long userNo) throws Exception {
+
+		List<Object[]> coinArrays = chargeCoinRepository.personalList(userNo);
+
+		List<ChargeCoin>  coinList = new ArrayList<ChargeCoin>();
+		for(Object[] valueArray : coinArrays) {
+			ChargeCoin chargeCoin = new ChargeCoin();
+
+			chargeCoin.setUserNo((Long)valueArray[0]);
+			chargeCoin.setAmount((int)valueArray[1]);
+			chargeCoin.setRegDate((LocalDateTime)valueArray[2]);
+			chargeCoin.setUpdDate((LocalDateTime)valueArray[3]);
+			chargeCoin.setUserNo((Long)valueArray[4]);;
+
+			coinList.add(chargeCoin);
+		}
+
+		return coinList;
+	}
+		
+	@Override
+	public List<PayCoin> listPayHistory(Long userNo) throws Exception {
+		List<Object[]> valueArrays = payCoinRepository.listPayHistory(userNo);
+		
+		List<PayCoin> payCoinList = new ArrayList<PayCoin>();
+		for(Object[] valueArray : valueArrays) {
+			PayCoin payCoin = new PayCoin();
+			
+			payCoin.setHistoryNo((Long)valueArray[0]);
+			payCoin.setUserNo((Long)valueArray[1]);
+			payCoin.setItemId((Long)valueArray[2]);
+			payCoin.setItemName((String)valueArray[3]);
+			payCoin.setAmount((int)valueArray[4]);
+			payCoin.setRegDate((LocalDateTime)valueArray[5]);
+			
+			payCoinList.add(payCoin);
+		}
+		
+		return payCoinList;
+	}
+	
+}
