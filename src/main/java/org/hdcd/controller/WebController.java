3593870ece35650c69ebb335c95c02d02317b9b6
rlxo4307 <rlxo4307.diquest.com>
2023-03-24 17:43:34 +0900

    Revert "03/24 부트스트랩 작업 초기로 복구"
    This reverts commit 8909f31d9dd6a7712fdbc0fb207daf6c37311c50.

--- src/main/java/org/hdcd/controller/WebController.java
+++ src/main/java/org/hdcd/controller/WebController.java
@@ -1,15 +1,15 @@
-//package org.hdcd.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class WebController {
-//
-//    @RequestMapping("/")
-//    public String main(){
-//
-//        return "index";
-//    }
-//
-//}
+package org.hdcd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/")
+    public String main(){
+
+        return "index";
+    }
+
+}
