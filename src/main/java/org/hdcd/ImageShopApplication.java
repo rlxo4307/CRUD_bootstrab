1fe331ce8366864421f64f2c19cf7413cb6a35e9
DIQUEST <@!WQSAXZ43re>
2023-03-28 14:27:31 +0900

    test(all): Mockito 사용 예제 - 데모 클래스 exclude 처리

--- src/main/java/org/hdcd/ImageShopApplication.java
+++ src/main/java/org/hdcd/ImageShopApplication.java
@@ -1,13 +1,17 @@
 package org.hdcd;
 
+import org.hdcd.mock.DemoInterface;
+import org.hdcd.mock.DemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {DemoInterface.class, DemoService.class})})
 public class ImageShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImageShopApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ImageShopApplication.class, args);
+  }
 }
