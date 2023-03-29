2b2a856af43c40d42af10c188b793e6013e79c5b
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-03-29 10:57:56 +0900

    Create textPopUp.js

---
+++ src/main/resources/static/js/textPopUp.js
@@ -0,0 +1,25 @@
+window.selectText();
+
+
+$( function() {
+    $( "#draggable" ).draggable();
+} );
+
+
+function selectText() {
+    var selectionText = ""; //마우스로 드래그한 글
+
+    if (document.getSelection) {
+    selectionText = document.getSelection();
+} else if (document.selection) {
+    selectionText = document.selection.createRange().text;
+}
+    return selectionText;
+}
+
+
+function dragPopUp(){
+    var popup = window.open("", '지식작업', 'width=400px,height=300px,scrollbars=yes');
+    getText = selectText();
+    popup.document.write(getText);
+}
