a8bcf27efb27ebd1457a0e512096b6b5feb73c3e
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-03-29 16:20:11 +0900

    Update textPopUp.js
    03/29 dragPopUp 함수, 전역변수 사용 x

--- src/main/resources/static/js/textPopUp.js
+++ src/main/resources/static/js/textPopUp.js
@@ -4,8 +4,6 @@ $( function() {
     $( "#draggable" ).draggable();
 } );
 
-var popup;
-
 function selectText() {
     var selectionText = ""; //마우스로 드래그한 글
 
@@ -18,8 +16,23 @@ function selectText() {
 }
 
 function dragPopUp(){
-    if(popup != null) popup.close();
-    popup = window.open("", "", 'width=400px,height=300px,scrollbars=yes');
+    // if(popup != null) popup.close();
+    var popup = window.open("", "", 'width=400px,height=300px,scrollbars=yes');
     var getText = selectText();
-    popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
+    if(popup.document==null) {
+        popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
+    } else if(!(popup.document==null)){
+        window.close();
+        popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
+    }
 }
+
+// var popup;
+
+// 전역변수 var popup 사용 시
+// function dragPopUp(){
+//     if(popup != null) popup.close();
+//     popup = window.open("", "", 'width=400px,height=300px,scrollbars=yes');
+//     var getText = selectText();
+//     popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
+// }
