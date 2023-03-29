51df4e69a56a70e8ea0cb497c48ae3e4535ef0b7
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-03-29 11:39:53 +0900

    Update textPopUp.js
    03/29 전역변수 사용으로 창 닫기

--- src/main/resources/static/js/textPopUp.js
+++ src/main/resources/static/js/textPopUp.js
@@ -4,6 +4,8 @@ $( function() {
     $( "#draggable" ).draggable();
 } );
 
+var popup;
+
 function selectText() {
     var selectionText = ""; //마우스로 드래그한 글
 
@@ -16,7 +18,8 @@ function selectText() {
 }
 
 function dragPopUp(){
-    var popup = window.open("", '지식작업', 'width=400px,height=300px,scrollbars=yes');
+    if(popup != null) popup.close();
+    popup = window.open("", '지식작업', 'width=400px,height=300px,scrollbars=yes');
     var getText = selectText();
     popup.document.write(getText);
 }
