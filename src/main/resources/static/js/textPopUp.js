1e3d727e293e993518650a54afb16550b31dc05e
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-03-29 17:09:27 +0900

    Update textPopUp.js
    03/29 주석 제거

--- src/main/resources/static/js/textPopUp.js
+++ src/main/resources/static/js/textPopUp.js
@@ -15,24 +15,11 @@ function selectText() {
     return selectionText;
 }
 
+var popup;
+
 function dragPopUp(){
-    // if(popup != null) popup.close();
-    var popup = window.open("", "", 'width=400px,height=300px,scrollbars=yes');
+    if(popup != null) popup.close();
+    popup = window.open("", "", 'width=400px,height=300px,scrollbars=yes');
     var getText = selectText();
-    if(popup.document==null) {
-        popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
-    } else if(!(popup.document==null)){
-        window.close();
-        popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
-    }
+    popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
 }
-
-// var popup;
-
-// 전역변수 var popup 사용 시
-// function dragPopUp(){
-//     if(popup != null) popup.close();
-//     popup = window.open("", "", 'width=400px,height=300px,scrollbars=yes');
-//     var getText = selectText();
-//     popup.document.write("<html><head><title>지식작업</title></head><body></body></html>", getText);
-// }
