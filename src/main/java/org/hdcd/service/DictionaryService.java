3b4331072ba51cc6a5234991c46c92ce983a5a40
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-07-05 17:33:53 +0900

    Update DictionaryService.java
    remove 함수 선언

--- src/main/java/org/hdcd/service/DictionaryService.java
+++ src/main/java/org/hdcd/service/DictionaryService.java
@@ -7,9 +7,10 @@ import org.springframework.security.core.Authentication;
 import java.util.List;
 
 public interface DictionaryService {
-//    public void insert(SiteUdic siteUdic) throws Exception;
 
     int insert(String word, String memo, Authentication authentication) throws Exception;
 
-    public List<SiteUdic> list(String userId) throws Exception;
+    int remove(String word, Authentication authentication) throws Exception;
+
+    List<SiteUdic> list(String userId) throws Exception;
 }
