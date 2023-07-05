c52c359276681375fbde4ff220316abe065e9598
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-07-05 11:35:51 +0900

    Create DictionaryRepository.java
    07/05 DictionaryRepository.java 생성

---
+++ src/main/java/org/hdcd/repository/DictionaryRepository.java
@@ -0,0 +1,19 @@
+package org.hdcd.repository;
+
+import org.hdcd.domain.ChargeCoin;
+import org.hdcd.domain.SiteUdic;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface DictionaryRepository extends JpaRepository<SiteUdic, String> {
+
+    @Query("SELECT _word, _memo, _user_id, _upDated "
+            + "FROM SiteUdic "
+            + "WHERE _user_id LIKE ?1 ")
+    public List<Object[]> personalDiclist(String userId);
+
+}
