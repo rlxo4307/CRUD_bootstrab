27494309a400ad5c2cf0501f6e7e48d7e1daa9b7
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-07-06 16:12:48 +0900

    Update DictionaryServiceImpl.java
    renew, modify

--- src/main/java/org/hdcd/service/DictionaryServiceImpl.java
+++ src/main/java/org/hdcd/service/DictionaryServiceImpl.java
@@ -36,16 +36,43 @@ public class DictionaryServiceImpl implements DictionaryService {
         }
 
         SiteUdic siteUdic = new SiteUdic();
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime currentTime = LocalDateTime.now();
 
         siteUdic.set_word(word);
         siteUdic.set_memo(memo);
         siteUdic.set_user_id(userId);
-        siteUdic.set_upDated(now);
+        siteUdic.set_up_dated(currentTime);
 
-        dictionaryRepository.save(siteUdic);
+//        dictionaryRepository.save(siteUdic);
+        dictionaryRepository.renew(word, memo, userId, currentTime);
+
         return 1;
     }
+
+//    @Override // 사용자 사전에 단어 추가
+//    public int insert(SiteUdic siteUdic) throws Exception {
+//        SiteUdic siteEntity = dictionaryRepository.getOne(siteUdic.get_user_id());
+//
+//        String userId = siteUdic.get_user_id();
+//        String word = siteUdic.get_word();
+//        String memo = siteUdic.get_memo();
+//
+//        List<SiteUdic> udicList = list(userId);
+//
+//        for(int i=0; i<udicList.size(); i++){
+//            if(word.equals(udicList.get(i).get_word())) {
+//                return 0;
+//            }
+//        }
+//
+//        LocalDateTime currentTime = LocalDateTime.now();
+//        siteUdic.set_up_dated(currentTime);
+//
+//        dictionaryRepository.save(siteUdic);
+////        dictionaryRepository.renew(word, memo, userId, currentTime);
+//
+//        return 1;
+//    }
 
     @Override
     public int remove(String word, Authentication authentication) throws Exception {
@@ -65,6 +92,32 @@ public class DictionaryServiceImpl implements DictionaryService {
         return 0;
     }
 
+    @Override
+    public int update (String word, String memo, Authentication authentication) throws Exception {
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        Member member = customUser.getMember();
+
+        String userId = member.getUserId();
+
+        List<SiteUdic> udicList = list(userId);
+
+        SiteUdic siteUdic = new SiteUdic();
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        siteUdic.set_word(word);
+        siteUdic.set_memo(memo);
+        siteUdic.set_user_id(userId);
+        siteUdic.set_up_dated(currentTime);
+
+        for(int i=0; i<udicList.size(); i++){
+            if(word.equals(udicList.get(i).get_word())) {
+                dictionaryRepository.personalDicUpdate(siteUdic);
+                return 1;
+            }
+        }
+        return 0;
+    }
+
     @Override // 사용자 사전 리스트
     public List<SiteUdic> list(String userId) throws Exception {
         List<Object[]> dicArrays = dictionaryRepository.personalDicList(userId);
@@ -77,7 +130,7 @@ public class DictionaryServiceImpl implements DictionaryService {
             siteUdic.set_word((String)valueArray[0]);
             siteUdic.set_memo((String)valueArray[1]);
             siteUdic.set_user_id((String)valueArray[2]);
-            siteUdic.set_upDated((LocalDateTime) valueArray[3]);
+            siteUdic.set_up_dated((LocalDateTime) valueArray[3]);
 
             udicList.add(siteUdic);
         }
