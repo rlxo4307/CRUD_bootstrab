32e35df96bc7d388362ce52dc390593002d64ad2
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-07-07 14:21:40 +0900

    Update SiteUdic.java

--- src/main/java/org/hdcd/domain/SiteUdic.java
+++ src/main/java/org/hdcd/domain/SiteUdic.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
