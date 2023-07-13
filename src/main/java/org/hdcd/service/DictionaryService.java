ae60b69e7896abf6a1ad1466038b439176db2ea4
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-07-13 17:25:50 +0900

    Update DictionaryService.java

--- src/main/java/org/hdcd/service/DictionaryService.java
+++ src/main/java/org/hdcd/service/DictionaryService.java
@@ -2,13 +2,14 @@ package org.hdcd.service;
 
 import org.hdcd.domain.SiteThesaurus;
 import org.hdcd.domain.SiteUdic;
+import org.hdcd.dto.SiteUdicDTO;
 import org.springframework.security.core.Authentication;
 
 import java.util.List;
 
 public interface DictionaryService {
 
-    public int insert(String word, String memo, Authentication authentication) throws Exception;
+    public int insert(SiteUdicDTO siteUdicDTO, Authentication authentication) throws Exception;
 
     public int remove(String word, Authentication authentication) throws Exception;
 
