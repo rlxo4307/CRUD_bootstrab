96ac2dc7fa5ad955925eaa1a3d35579a630863a3
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-07-14 09:34:01 +0900

    Update SiteUdicDTO.java

--- src/main/java/org/hdcd/dto/SiteUdicDTO.java
+++ src/main/java/org/hdcd/dto/SiteUdicDTO.java
@@ -5,14 +5,10 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Data
-@Getter
-@Setter
-@ToString
 @AllArgsConstructor
 public class SiteUdicDTO {
     private String _word;
     private String _memo;
     private String _user_id;
     private LocalDateTime _up_dated;
-
 }
