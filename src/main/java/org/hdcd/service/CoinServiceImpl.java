80a7e7768016fc99cf4f2520ddc0ac147b3f7e48
hankitae <rlxo4307@gmail.com>
2023-07-15 20:15:07 +0900

    금요일 덮어쓰기 오류 정정

--- src/main/java/org/hdcd/service/CoinServiceImpl.java
+++ src/main/java/org/hdcd/service/CoinServiceImpl.java
@@ -1,126 +1,95 @@
 package org.hdcd.service;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.time.LocalDateTime;
+import java.util.List;
 
-import org.hdcd.common.security.domain.CustomUser;
+import org.hdcd.domain.ChargeCoin;
 import org.hdcd.domain.Member;
-import org.hdcd.domain.SiteUdic;
-import org.hdcd.repository.DictionaryRepository;
-import org.springframework.security.core.Authentication;
+import org.hdcd.domain.PayCoin;
+import org.hdcd.repository.ChargeCoinRepository;
+import org.hdcd.repository.MemberRepository;
+import org.hdcd.repository.PayCoinRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-
 @RequiredArgsConstructor
 @Service
-public class DictionaryServiceImpl implements DictionaryService {
+public class CoinServiceImpl implements CoinService {
 
-    private final DictionaryRepository dictionaryRepository;
+    private final ChargeCoinRepository chargeCoinRepository;
+
+    private final PayCoinRepository payCoinRepository;
+
+    private final MemberRepository memberRepository;
 
     @Transactional
-    @Override // 사용자 사전에 단어 추가
-    public int insert(String word, String memo, Authentication authentication) throws Exception {
-        CustomUser customUser = (CustomUser) authentication.getPrincipal();
-        Member member = customUser.getMember();
+    @Override
+    public void charge(ChargeCoin chargeCoin) throws Exception {
+        Member memberEntity = memberRepository.getOne(chargeCoin.getUserNo());
 
-        String userId = member.getUserId();
+        int coin = memberEntity.getCoin();
+        int amount = chargeCoin.getAmount();
 
-        List<SiteUdic> udicList = list(userId);
+        memberEntity.setCoin(coin + amount);
 
-        for(int i=0; i<udicList.size(); i++){
-            if(word.equals(udicList.get(i).get_word())) {
-                return 0;
-            }
-        }
+        memberRepository.save(memberEntity);
 
-//        SiteUdic siteUdic = new SiteUdic();
-        LocalDateTime currentTime = LocalDateTime.now();
-
-//        siteUdic.set_word(word);
-//        siteUdic.set_memo(memo);
-//        siteUdic.set_user_id(userId);
-//        siteUdic.set_up_dated(currentTime);
-
-        try {
-            dictionaryRepository.renew(word, memo, userId, currentTime);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-//        try {
-//            dictionaryRepository.save(siteUdic);
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-        return 1;
+        chargeCoinRepository.save(chargeCoin);
     }
 
-
-    @Override // 사용자 사전의 단어 제거
-    public int remove(String word, Authentication authentication) throws Exception {
-        CustomUser customUser = (CustomUser) authentication.getPrincipal();
-        Member member = customUser.getMember();
-
-        String userId = member.getUserId();
-
-        List<SiteUdic> udicList = list(userId);
-
-        for(int i=0; i<udicList.size(); i++){
-            if(word.equals(udicList.get(i).get_word())) {
-                dictionaryRepository.deleteById(word);
-                return 1;
-            }
-        }
-        return 0;
+    @Override
+    public List<ChargeCoin> list(Long userNo) throws Exception {
+        return chargeCoinRepository.findAll(Sort.by(Direction.DESC, "historyNo"));
     }
 
+    @Override
+    public List<ChargeCoin> personalList(Long userNo) throws Exception {
 
-    @Override // 사용자 사전 단어 업데이트
-    public int update (String word, String memo, Authentication authentication) throws Exception {
-        CustomUser customUser = (CustomUser) authentication.getPrincipal();
-        Member member = customUser.getMember();
+        List<Object[]> coinArrays = chargeCoinRepository.personalList(userNo);
 
-        String userId = member.getUserId();
+        List<ChargeCoin> coinList = new ArrayList<ChargeCoin>();
+        long rowNum = coinArrays.stream().count();
 
-        List<SiteUdic> udicList = list(userId);
+        for (Object[] valueArray : coinArrays) {
+            ChargeCoin chargeCoin = new ChargeCoin();
 
-        LocalDateTime currentTime = LocalDateTime.now();
+//			chargeCoin.setHistoryNo((Long)valueArray[0]);
+            chargeCoin.setHistoryNo(rowNum--);
+            chargeCoin.setAmount((int) valueArray[1]);
+            chargeCoin.setRegDate((LocalDateTime) valueArray[2]);
 
-        for(int i=0; i<udicList.size(); i++){
-            if(word.equals(udicList.get(i).get_word())) {
-                try {
-                    dictionaryRepository.personalDicUpdate(word, memo, userId, currentTime);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                return 1;
-            }
-        }
-        return 0;
-    }
-
-
-    @Override // 사용자 사전 리스트
-    public List<SiteUdic> list(String userId) throws Exception {
-        List<Object[]> dicArrays = dictionaryRepository.personalDicList(userId);
-
-        List<SiteUdic> udicList = new ArrayList<>();
-
-        for(Object[] valueArray : dicArrays) {
-            SiteUdic siteUdic = new SiteUdic();
-
-            siteUdic.set_word((String)valueArray[0]);
-            siteUdic.set_memo((String)valueArray[1]);
-            siteUdic.set_user_id((String)valueArray[2]);
-            siteUdic.set_up_dated((LocalDateTime) valueArray[3]);
-
-            udicList.add(siteUdic);
+            coinList.add(chargeCoin);
         }
 
-        return udicList;
+        return coinList;
     }
 
+    @Override
+    public List<PayCoin> listPayHistory(Long userNo) throws Exception {
+        List<Object[]> valueArrays = payCoinRepository.listPayHistory(userNo);
+
+        List<PayCoin> payCoinList = new ArrayList<PayCoin>();
+        long rowNum = valueArrays.stream().count();
+
+        for (Object[] valueArray : valueArrays) {
+            PayCoin payCoin = new PayCoin();
+
+//			payCoin.setHistoryNo((Long)valueArray[0]);
+            payCoin.setHistoryNo(rowNum--);
+            payCoin.setUserNo((Long) valueArray[1]);
+            payCoin.setItemId((Long) valueArray[2]);
+            payCoin.setItemName((String) valueArray[3]);
+            payCoin.setAmount((int) valueArray[4]);
+            payCoin.setRegDate((LocalDateTime) valueArray[5]);
+
+            payCoinList.add(payCoin);
+        }
+
+        return payCoinList;
+    }
 }
