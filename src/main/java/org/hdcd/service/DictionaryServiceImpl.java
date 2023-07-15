994582eeee9d0285270e05bdb8dea00451c76498
hankitae <rlxo4307@gmail.com>
2023-07-15 20:19:35 +0900

    DictionaryServiceImpl 재생성

--- src/main/java/org/hdcd/service/DictionaryServiceImpl.java
+++ src/main/java/org/hdcd/service/DictionaryServiceImpl.java
@@ -21,14 +21,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Service
-public class DictionaryServiceImpl implements DictionaryService {
+public class  DictionaryServiceImpl implements DictionaryService {
 
-    private final DictionaryRepository dictionaryRepository;
-    LocalDateTime currentTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+private final DictionaryRepository dictionaryRepository;
+        LocalDateTime currentTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
 
-    @Transactional
-    @Override // 사용자 사전에 단어 추가
-    public int insert(SiteUdic siteUdic, Authentication authentication) throws Exception {
+@Transactional
+@Override // 사용자 사전에 단어 추가
+public int insert(SiteUdic siteUdic, Authentication authentication) throws Exception {
         CustomUser customUser = (CustomUser) authentication.getPrincipal();
         Member member = customUser.getMember();
 
@@ -40,23 +40,23 @@ public class DictionaryServiceImpl implements DictionaryService {
         List<SiteUdic> udicList = list_uDic(userId);
 
         for(int i=0; i<udicList.size(); i++){
-            if(word.equals(udicList.get(i).get_word())) {
-                return 0;
-            }
+        if(word.equals(udicList.get(i).get_word())) {
+        return 0;
+        }
         }
 
         try {
-            dictionaryRepository.renew(word, memo, userId, currentTime);
+        dictionaryRepository.renew(word, memo, userId, currentTime);
         }catch (Exception e){
-            e.printStackTrace();
+        e.printStackTrace();
         }
 
         return 1;
-    }
+        }
 
 
-    @Override // 사용자 사전의 단어 제거
-    public int remove(String word, Authentication authentication) throws Exception {
+@Override // 사용자 사전의 단어 제거
+public int remove(String word, Authentication authentication) throws Exception {
         CustomUser customUser = (CustomUser) authentication.getPrincipal();
         Member member = customUser.getMember();
 
@@ -65,16 +65,16 @@ public class DictionaryServiceImpl implements DictionaryService {
         List<SiteUdic> udicList = list_uDic(userId);
 
         for(int i=0; i<udicList.size(); i++){
-            if(word.equals(udicList.get(i).get_word())) {
-                dictionaryRepository.personaluDicDelete(word);
-                return 1;
-            }
+        if(word.equals(udicList.get(i).get_word())) {
+        dictionaryRepository.personaluDicDelete(word);
+        return 1;
+        }
         }
         return 0;
-    }
-    @Transactional
-    @Override // 사용자 사전의 단어 제거
-    public int checkedRemoveThesaurus(SiteThesaurusDTO siteThesaurusDTO, Authentication authentication) throws Exception {
+        }
+@Transactional
+@Override // 사용자 사전의 단어 제거
+public int checkedRemoveThesaurus(SiteThesaurusDTO siteThesaurusDTO, Authentication authentication) throws Exception {
 
         CustomUser customUser = (CustomUser) authentication.getPrincipal();
         Member member = customUser.getMember();
@@ -88,23 +88,23 @@ public class DictionaryServiceImpl implements DictionaryService {
         String[] splitWord = Str.split(",");
 
         for (int i=0; i < splitWord.length; i++) {
-            wordlist.add(splitWord[i]);
+        wordlist.add(splitWord[i]);
         }
 
         int wordSize = wordlist.size();
         int count = 0;
 
         for (int i = 0; i < wordlist.size(); i++) {
-            String word = wordlist.get(i);
-            if (dicList.contains(word)) {
-                dictionaryRepository.personalThesaurusDelete(word);
-                count++;
-            }
+        String word = wordlist.get(i);
+        if (dicList.contains(word)) {
+        dictionaryRepository.personalThesaurusDelete(word);
+        count++;
+        }
         }
         if(count == wordSize) return 1;
 
         return 0;
-    }
+        }
 
 //    @Transactional
 //    @Override // 사용자 사전의 단어 제거
@@ -141,9 +141,9 @@ public class DictionaryServiceImpl implements DictionaryService {
 //    }
 
 
-    @Transactional
-    @Override // 사용자 사전의 단어 제거
-    public int checkedRemoveUdic(@RequestParam(value="wordList[]") List<String> wordList, Authentication authentication) throws Exception {
+@Transactional
+@Override // 사용자 사전의 단어 제거
+public int checkedRemoveUdic(@RequestParam(value="wordList[]") List<String> wordList, Authentication authentication) throws Exception {
 
         CustomUser customUser = (CustomUser) authentication.getPrincipal();
         Member member = customUser.getMember();
@@ -155,20 +155,20 @@ public class DictionaryServiceImpl implements DictionaryService {
         int count = 0;
 
         for (int i = 0; i < wordList.size(); i++) {
-            String word = wordList.get(i);
-            if (dicList.contains(word)) {
-                dictionaryRepository.personaluDicDelete(word);
-                count++;
-            }
+        String word = wordList.get(i);
+        if (dicList.contains(word)) {
+        dictionaryRepository.personaluDicDelete(word);
+        count++;
+        }
         }
         if(count == wordSize) return 1;
 
         return 0;
-    }
+        }
 
 
-    @Override // 사용자 사전 단어 업데이트
-    public int update (String word, String memo, Authentication authentication) throws Exception {
+@Override // 사용자 사전 단어 업데이트
+public int update (String word, String memo, Authentication authentication) throws Exception {
         CustomUser customUser = (CustomUser) authentication.getPrincipal();
         Member member = customUser.getMember();
 
@@ -177,59 +177,59 @@ public class DictionaryServiceImpl implements DictionaryService {
         List<SiteUdic> udicList = list_uDic(userId);
 
         for(int i=0; i<udicList.size(); i++){
-            if(word.equals(udicList.get(i).get_word())) {
-                try {
-                    dictionaryRepository.personalDicUpdate(word, memo, currentTime);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                return 1;
-            }
+        if(word.equals(udicList.get(i).get_word())) {
+        try {
+        dictionaryRepository.personalDicUpdate(word, memo, currentTime);
+        }catch (Exception e){
+        e.printStackTrace();
+        }
+        return 1;
+        }
         }
         return 0;
-    }
+        }
 
 
-    @Override // 사용자 사전 리스트
-        public List<SiteUdic> list_uDic(String userId) throws Exception {
-            List<Object[]> dicArrays = dictionaryRepository.personalDicList_uDic(userId);
+@Override // 사용자 사전 리스트
+public List<SiteUdic> list_uDic(String userId) throws Exception {
+        List<Object[]> dicArrays = dictionaryRepository.personalDicList_uDic(userId);
 
-            List<SiteUdic> udicList = new ArrayList<>();
+        List<SiteUdic> udicList = new ArrayList<>();
 
-            for(Object[] valueArray : dicArrays) {
-                SiteUdic siteUdic = new SiteUdic();
+        for(Object[] valueArray : dicArrays) {
+        SiteUdic siteUdic = new SiteUdic();
 
-                siteUdic.set_word((String)valueArray[0]);
-                siteUdic.set_memo((String)valueArray[1]);
-                siteUdic.set_user_id((String)valueArray[2]);
-                siteUdic.set_up_dated((LocalDateTime) valueArray[3]);
+        siteUdic.set_word((String)valueArray[0]);
+        siteUdic.set_memo((String)valueArray[1]);
+        siteUdic.set_user_id((String)valueArray[2]);
+        siteUdic.set_up_dated((LocalDateTime) valueArray[3]);
 
-                udicList.add(siteUdic);
-            }
+        udicList.add(siteUdic);
+        }
 
-            return udicList;
-    }
+        return udicList;
+        }
 
 
-    @Override // 사용자 사전 리스트
-    public List<SiteThesaurus> list_thesaurus(String userId) throws Exception {
+@Override // 사용자 사전 리스트
+public List<SiteThesaurus> list_thesaurus(String userId) throws Exception {
         List<Object[]> dicArrays = dictionaryRepository.personalDicList_thesaurus(userId);
 
         List<SiteThesaurus> thesaurusList = new ArrayList<>();
 
         for(Object[] valueArray : dicArrays) {
-            SiteThesaurus siteThesaurus = new SiteThesaurus();
+        SiteThesaurus siteThesaurus = new SiteThesaurus();
 
-            siteThesaurus.set_head_word((String)valueArray[0]);
-            siteThesaurus.set_tail_word((String)valueArray[1]);
-            siteThesaurus.set_memo((String)valueArray[2]);
-            siteThesaurus.set_user_id((String)valueArray[3]);
-            siteThesaurus.set_up_dated((LocalDateTime) valueArray[4]);
+        siteThesaurus.set_head_word((String)valueArray[0]);
+        siteThesaurus.set_tail_word((String)valueArray[1]);
+        siteThesaurus.set_memo((String)valueArray[2]);
+        siteThesaurus.set_user_id((String)valueArray[3]);
+        siteThesaurus.set_up_dated((LocalDateTime) valueArray[4]);
 
-            thesaurusList.add(siteThesaurus);
+        thesaurusList.add(siteThesaurus);
         }
 
         return thesaurusList;
-    }
+        }
 
-}
+        }
