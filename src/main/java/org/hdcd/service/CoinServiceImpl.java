1ec89f4d0eb4f05a4969f4a52c905ba9d094d596
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2023-07-17 09:33:25 +0900

    Update CoinServiceImpl.java

--- src/main/java/org/hdcd/service/CoinServiceImpl.java
+++ src/main/java/org/hdcd/service/CoinServiceImpl.java
@@ -12,84 +12,85 @@ import org.hdcd.repository.MemberRepository;
 import org.hdcd.repository.PayCoinRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @Service
 public class CoinServiceImpl implements CoinService {
 
-    private final ChargeCoinRepository chargeCoinRepository;
+	private final ChargeCoinRepository chargeCoinRepository;
+	
+	private final PayCoinRepository payCoinRepository;
+	
+	private final MemberRepository memberRepository;
+	
+	@Transactional
+	@Override
+	public void charge(ChargeCoin chargeCoin) throws Exception {
+		Member memberEntity = memberRepository.getOne(chargeCoin.getUserNo());
+		
+		int coin = memberEntity.getCoin();
+		int amount = chargeCoin.getAmount();
+		
+		memberEntity.setCoin(coin + amount);
+	
+		memberRepository.save(memberEntity);
+		
+		chargeCoinRepository.save(chargeCoin);
+	}
 
-    private final PayCoinRepository payCoinRepository;
+	@Override
+	public List<ChargeCoin> list(Long userNo) throws Exception {
+		return chargeCoinRepository.findAll(Sort.by(Direction.DESC, "historyNo"));
+	}
 
-    private final MemberRepository memberRepository;
+	@Override
+	public List<ChargeCoin> personalList(Long userNo) throws Exception {
 
-    @Transactional
-    @Override
-    public void charge(ChargeCoin chargeCoin) throws Exception {
-        Member memberEntity = memberRepository.getOne(chargeCoin.getUserNo());
+		List<Object[]> coinArrays = chargeCoinRepository.personalList(userNo);
 
-        int coin = memberEntity.getCoin();
-        int amount = chargeCoin.getAmount();
+		List<ChargeCoin>  coinList = new ArrayList<ChargeCoin>();
+		long rowNum = coinArrays.stream().count();
 
-        memberEntity.setCoin(coin + amount);
-
-        memberRepository.save(memberEntity);
-
-        chargeCoinRepository.save(chargeCoin);
-    }
-
-    @Override
-    public List<ChargeCoin> list(Long userNo) throws Exception {
-        return chargeCoinRepository.findAll(Sort.by(Direction.DESC, "historyNo"));
-    }
-
-    @Override
-    public List<ChargeCoin> personalList(Long userNo) throws Exception {
-
-        List<Object[]> coinArrays = chargeCoinRepository.personalList(userNo);
-
-        List<ChargeCoin> coinList = new ArrayList<ChargeCoin>();
-        long rowNum = coinArrays.stream().count();
-
-        for (Object[] valueArray : coinArrays) {
-            ChargeCoin chargeCoin = new ChargeCoin();
+		for(Object[] valueArray : coinArrays) {
+			ChargeCoin chargeCoin = new ChargeCoin();
 
 //			chargeCoin.setHistoryNo((Long)valueArray[0]);
-            chargeCoin.setHistoryNo(rowNum--);
-            chargeCoin.setAmount((int) valueArray[1]);
-            chargeCoin.setRegDate((LocalDateTime) valueArray[2]);
+			chargeCoin.setHistoryNo(rowNum--);
+			chargeCoin.setAmount((int)valueArray[1]);
+			chargeCoin.setRegDate((LocalDateTime)valueArray[2]);
 
-            coinList.add(chargeCoin);
-        }
+			coinList.add(chargeCoin);
+		}
 
-        return coinList;
-    }
+		return coinList;
+	}
+		
+	@Override
+	public List<PayCoin> listPayHistory(Long userNo) throws Exception {
+		List<Object[]> valueArrays = payCoinRepository.listPayHistory(userNo);
+		
+		List<PayCoin> payCoinList = new ArrayList<PayCoin>();
+		long rowNum = valueArrays.stream().count();
 
-    @Override
-    public List<PayCoin> listPayHistory(Long userNo) throws Exception {
-        List<Object[]> valueArrays = payCoinRepository.listPayHistory(userNo);
-
-        List<PayCoin> payCoinList = new ArrayList<PayCoin>();
-        long rowNum = valueArrays.stream().count();
-
-        for (Object[] valueArray : valueArrays) {
-            PayCoin payCoin = new PayCoin();
-
+		for(Object[] valueArray : valueArrays) {
+			PayCoin payCoin = new PayCoin();
+			
 //			payCoin.setHistoryNo((Long)valueArray[0]);
-            payCoin.setHistoryNo(rowNum--);
-            payCoin.setUserNo((Long) valueArray[1]);
-            payCoin.setItemId((Long) valueArray[2]);
-            payCoin.setItemName((String) valueArray[3]);
-            payCoin.setAmount((int) valueArray[4]);
-            payCoin.setRegDate((LocalDateTime) valueArray[5]);
-
-            payCoinList.add(payCoin);
-        }
-
-        return payCoinList;
-    }
+			payCoin.setHistoryNo(rowNum--);
+			payCoin.setUserNo((Long)valueArray[1]);
+			payCoin.setItemId((Long)valueArray[2]);
+			payCoin.setItemName((String)valueArray[3]);
+			payCoin.setAmount((int)valueArray[4]);
+			payCoin.setRegDate((LocalDateTime)valueArray[5]);
+			
+			payCoinList.add(payCoin);
+		}
+		
+		return payCoinList;
+	}
+	
 }
